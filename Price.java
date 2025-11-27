d27dafc17b0a220be3b08cc13655fd31de477689
Fmacielll <ffrmaciel@minha.fag.edu.br>
2025-11-26 23:43:11 -0300

    Commit 15

--- Price.java
+++ Price.java
@@ -2,9 +2,8 @@ public abstract class Price {
     public abstract int getPriceCode();
     public abstract double getCharge(int daysRented);
 
-    // Método extraído de Movie
+    // comportamento padrão
     public int getFrequentRenterPoints(int daysRented) {
-        // comportamento padrão
         return 1;
     }
 }
