d55c977accb5762fd65d2a5ba2d85deb32928c01
Fmacielll <ffrmaciel@minha.fag.edu.br>
2025-11-26 23:36:36 -0300

    Commit 12

--- Movie.java
+++ Movie.java
@@ -4,7 +4,7 @@ public class Movie {
     public static final int CHILDRENS = 2;
 
     private String _title;
-    private Price _price; // novo campo
+    private Price _price;
 
     public Movie(String title, int priceCode) {
         _title = title;
@@ -35,34 +35,12 @@ public class Movie {
         }
     }
 
+    // Agora apenas delega para Price
     public double getCharge(int daysRented) {
-        double result = 0;
-
-        switch (getPriceCode()) {
-            case REGULAR:
-                result += 2;
-                if (daysRented > 2)
-                    result += (daysRented - 2) * 1.5;
-                break;
-
-            case NEW_RELEASE:
-                result += daysRented * 3;
-                break;
-
-            case CHILDRENS:
-                result += 1.5;
-                if (daysRented > 3)
-                    result += (daysRented - 3) * 1.5;
-                break;
-        }
-
-        return result;
+        return _price.getCharge(daysRented);
     }
 
     public int getFrequentRenterPoints(int daysRented) {
-        if (getPriceCode() == NEW_RELEASE && daysRented > 1) {
-            return 2;
-        }
-        return 1;
+        return _price.getFrequentRenterPoints(daysRented);
     }
 }
