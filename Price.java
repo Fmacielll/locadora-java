35bffafa60af7447dbe53fad743944d66452e985
Fmacielll <ffrmaciel@minha.fag.edu.br>
2025-11-26 23:39:16 -0300

    Commit 13

--- Price.java
+++ Price.java
@@ -1,32 +1,8 @@
 public abstract class Price {
     public abstract int getPriceCode();
+    public abstract double getCharge(int daysRented);
 
-    // Novo método extraído
-    public double getCharge(int daysRented) {
-        double result = 0;
-
-        switch (getPriceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (daysRented > 2)
-                    result += (daysRented - 2) * 1.5;
-                break;
-
-            case Movie.NEW_RELEASE:
-                result += daysRented * 3;
-                break;
-
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (daysRented > 3)
-                    result += (daysRented - 3) * 1.5;
-                break;
-        }
-
-        return result;
-    }
-
-    // comportamento padrão (vai ser refinado no Commit 15)
+    // comportamento padrão (vai ser refinado depois no Commit 15)
     public int getFrequentRenterPoints(int daysRented) {
         return 1;
     }
